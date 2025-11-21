ebc69f226d1a140413910d85006ef6587119f2d5
Obada joba <Jobaobada8@gmail.com>
2025-11-21 17:10:16 +0200

    My template think for cp 1 version (Sublime template)

---
+++ template1.cpp
@@ -0,0 +1,41 @@
+<snippet>
+    <content><![CDATA[
+// Author: obada joba ==== Date: $CURRENT_DATETIME
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#pragma GCC optimize("Ofast")
+using namespace __gnu_pbds;
+using namespace std;
+const long long N = 1e5 + 5 ,  MOD = 1e9 + 7 , inf = 2e18 + 10 ;
+#define endl '\n'
+#define int long long
+#define double long double
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve() {
+    $0
+
+}
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t = 1;
+    //cin >> t;
+    cerr << "Run right code O.J" << endl ;
+    while (t--) {
+        solve();
+        // if (t) cout << '\n';
+    }
+
+    return 0;
+}
+]]></content>
+    <tabTrigger>think</tabTrigger>
+    <!-- <scope>source.cpp</scope> -->
+    <!-- <description>C++ Competitive Programming Template with Header</description> -->
+</snippet>
