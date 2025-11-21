ca43caa611fa578662d4a94ff3f85df7a95046a3
Obada joba <Jobaobada8@gmail.com>
2025-11-21 17:29:16 +0200

    prefix sum implementation (range sum queries)

---
+++ topics/Prefix Sum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+
+    vector<long long> a(n + 1, 0);
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    for (int i = 1; i <= n; i++)
+        a[i] += a[i - 1];
+
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+        cout << a[r] - a[l - 1] << endl;
+    }
+
+    return 0;
+}
