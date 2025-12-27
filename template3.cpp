68bd5563bb2172caf1c15dda61d08e4f7348a49e
Obada joba <Jobaobada8@gmail.com>
2025-12-27 18:32:15 +0200

    New Template for contests

---
+++ template3.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define per(i, a, b) for(int i = (b) - 1; i >= (a); --i)
+#define trav(a, x) for(auto& a : x)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+template<typename T> void ckmin(T& a, const T& b) { a = min(a, b); }
+template<typename T> void ckmax(T& a, const T& b) { a = max(a, b); }
+template<typename T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template<typename T> T lcm(T a, T b) { return a / gcd(a, b) * b; }
+template<typename T> T ceil_div(T a, T b) { return (a + b - 1) / b; }
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9;
+constexpr ll LINF = 1e18;
+constexpr ld EPS = 1e-9;
+constexpr int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+constexpr int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+
+ll power(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    a %= mod;
+    while(b > 0) {
+        if(b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll modinv(ll a, ll mod = MOD) {
+    return power(a, mod - 2, mod);
+}
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
